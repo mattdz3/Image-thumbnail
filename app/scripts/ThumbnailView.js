2f0732f7e031dd4b9e37716c6a0dabc3506c2197
Matt Dougherty <mattz325@gmail.com>
2014-06-04 21:50:51 -0400

    server is broken

--- app/scripts/ThumbnailView.js
+++ app/scripts/ThumbnailView.js
@@ -1,5 +1,45 @@
 "use strict";
 
 var ThumbnailView = Backbone.View.extend({
+
+	className: 'thumbnail',
 	
+	template: _.template($('.thumbnail-image').text()),
+	detailTemplate: _.template($('.detail-image').text()),
+
+	events: {
+		"click" : "showDetailView",
+	},
+
+	initialize: function() {
+		
+		$('.container').append(this.el);
+		this.render();
+	},
+
+	render: function() {
+		var renderTemp = this.template(this.model.attributes)
+		this.$el.html(renderTemp);
+	},
+
+	showDetailView: function() {
+		var renderTemp = this.detailTemplate(this.model.attributes)
+		this.$el.html(renderTemp);
+
+		new DetailView({model: this.model})
+	},
+
+
+
+
+
+
+})
+
+var getPhotos = new PhotoCollection();
+
+getPhotos.fetch().done(function() {
+	getPhotos.each(function(photos) {
+		new ThumbnailView({model: photos});
+	})
 })
--- app/scripts/mainView.js
+++ app/scripts/mainView.js
@@ -1,5 +1,42 @@
 "use strict";
 
 var DetailView = Backbone.View.extend({
-	
+
+	className: "detail-pic",
+
+	template: _.template($('.thumbnail-image').text()),
+	detailTemplate: _.template($('.detail-image').text()),
+
+	events: {
+		"click" : "goGetPhoto",
+		"click .detail-image" : "showThumbnailView",
+	},
+
+	initialize: function() {
+		$('.container').empty();
+		$('.container').append(this.el);
+		this.render();
+	},
+
+	render: function() {
+		var renderTemp = this.detailTemplate(this.model.attributes)
+		this.$el.html(renderTemp);
+	},
+
+	showThumbnailView: function() {
+		var renderTemp = this.template(this.model.attributes)
+		this.$el.html(renderTemp);
+
+		new ThumbnailView({model: this.model})
+	},
+
+	goGetPhoto: function() {
+		var getPhoto = new PhotoCollection();
+
+		getPhoto.get('url').done(function() {
+			new DetailView({model: this.model})
+		})
+	},
+
 })
+
